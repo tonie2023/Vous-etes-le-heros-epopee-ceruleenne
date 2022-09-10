eb6bf08a801200e085bdf178c047ac741a060d2c
tonie2023 <tony_19m@hotmail.ca>
2022-09-10 15:32:52 -0400

    Création du chapitre Obj

---
+++ javascript.js
@@ -0,0 +1,289 @@
+let chapterObj = {
+    prologue: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    depart: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    equipage: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    premier_choix: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    deuxieme_choix: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    embranchement_premier: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    deuxieme_ile: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    premier_objet: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    premiere_mort: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    embranchement_deuxieme: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    seconde_ile: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    premier_objet_un: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    premiere_mort_un: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    deuxieme_epreuve: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    ile_finale: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    second_objet: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    deuxieme_mort: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    seconde_epreuve: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    ile_tresor: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    deuxieme_objet: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    seconde_mort: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    epreuve_finale: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    tresor: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    recommencer: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    obtention_objets: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    richesse: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    reprendre: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+},
+    tresor_retrouve: {
+        subtitle:,
+        text:,
+        img:,
+        options:[
+            {
+
+            
+    },],
+    epilogue: {
+        subtitle:,
+        text:,
+        img:,
+    },
+};
+function GoToChapter(chapterName) {
+  const Chapitres = chapterName;
+}
