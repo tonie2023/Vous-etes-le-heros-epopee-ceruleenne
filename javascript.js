dba1bc7a792844ec64a9137d958689b5841c6891
tonie2023 <tony_19m@hotmail.ca>
2022-09-10 16:02:49 -0400

    Ajout

--- javascript.js
+++ javascript.js
@@ -1,289 +1,182 @@
 let chapterObj = {
-    prologue: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    depart: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    equipage: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    premier_choix: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    deuxieme_choix: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    embranchement_premier: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    deuxieme_ile: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    premier_objet: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    premiere_mort: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    embranchement_deuxieme: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    seconde_ile: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    premier_objet_un: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    premiere_mort_un: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    deuxieme_epreuve: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    ile_finale: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    second_objet: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    deuxieme_mort: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    seconde_epreuve: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    ile_tresor: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    deuxieme_objet: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    seconde_mort: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    epreuve_finale: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    tresor: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    recommencer: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    obtention_objets: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    richesse: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    reprendre: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-},
-    tresor_retrouve: {
-        subtitle:,
-        text:,
-        img:,
-        options:[
-            {
-
-            
-    },],
-    epilogue: {
-        subtitle:,
-        text:,
-        img:,
-    },
+  prologue: {
+    subtitle: "Éveil du capitaine",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  depart: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  equipage: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  premier_choix: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  deuxieme_choix: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  embranchement_premier: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  deuxieme_ile: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  premier_objet: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  premiere_mort: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  embranchement_deuxieme: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  seconde_ile: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  premier_objet_un: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  premiere_mort_un: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  deuxieme_epreuve: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  ile_finale: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  second_objet: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  deuxieme_mort: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  seconde_epreuve: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  ile_tresor: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  deuxieme_objet: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  seconde_mort: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  epreuve_finale: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  tresor: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  recommencer: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  obtention_objets: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  richesse: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  reprendre: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  tresor_retrouve: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+    options: [{}],
+  },
+  epilogue: {
+    subtitle: "",
+    text: ``,
+    img: "assets/",
+  },
 };
-function GoToChapter(chapterName) {
-  const Chapitres = chapterName;
+function GoToChapter(chapterObj) {
+  console.log(chapterObj[chapterName].subtitle);
+  console.log(chapterObj[chapterName].text);
+  console.log(chapterObj[chapterName].img);
+  console.log(chapterObj[chapterName].options);
 }
+GoToChapter();
